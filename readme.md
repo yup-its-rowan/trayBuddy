3a3739285fa95d10028df5b3375297ccfc4ffc75
Rohan Akki <roboy13@yahoo.com>
2022-05-09 16:18:24 -0400

    Added a good amount of functionality. Will add more when I feel it.

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-This is a little program I keep running to help automate tasks I do a lot. Plans to interface this with hardware optimizations and side projects as I work on those.
+This is a little program I keep running to help automate tasks I do a lot. Plans to create a website that's controllable by this, and then connect hardware to the website. Hopefully given time I'll be able to make my own web of connected devices I control through this.
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,11 @@ package com.company;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Main {
 
@@ -16,31 +18,73 @@ public class Main {
         Image image = ImageIO.read(new File("src/com/company/icon.png"));
         final PopupMenu popup = new PopupMenu();
         final TrayIcon trayIcon = new TrayIcon(image);
-        trayIcon.setImageAutoSize(true);
         final SystemTray tray = SystemTray.getSystemTray();
+        trayIcon.setImageAutoSize(true);
+        Desktop desktop = Desktop.getDesktop();
 
         // Create a pop-up menu components
-        MenuItem aboutItem = new MenuItem("About");
-        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
-        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
-        Menu displayMenu = new Menu("Display");
-        MenuItem errorItem = new MenuItem("Error");
-        MenuItem warningItem = new MenuItem("Warning");
-        MenuItem infoItem = new MenuItem("Info");
-        MenuItem noneItem = new MenuItem("None");
+        //MenuItem aboutItem = new MenuItem("About");
+        //CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
+        //CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
+        Menu shortcutsMenu = new Menu("Shortcuts");
+            MenuItem emailItem = new MenuItem("Email");
+            MenuItem schoolItem = new MenuItem("School");
+            MenuItem tf2Item = new MenuItem("TF2");
         MenuItem exitItem = new MenuItem("Exit");
 
+        //mess with menu items
+        ActionListener exitListener = e -> tray.remove(trayIcon);
+        exitItem.addActionListener(exitListener);
+
+        ActionListener emailListener = e -> {
+            try {
+                desktop.browse(new URI("https://mail.google.com/mail/u/0/#inbox"));
+                desktop.browse(new URI("https://mail.yahoo.com/d/folders/1"));
+                desktop.browse(new URI("https://outlook.office365.com/mail/"));
+            } catch (IOException | URISyntaxException ioException) {
+                ioException.printStackTrace();
+            }
+
+        };
+        emailItem.addActionListener(emailListener);
+
+        ActionListener schoolListener = e -> {
+            try {
+                desktop.browse(new URI("https://sakai.unc.edu/portal/site/1002d5b2-6fae-498d-981c-ec89bd586fc2"));
+                desktop.browse(new URI("https://www.gradescope.com"));
+            } catch (IOException | URISyntaxException ioException) {
+                ioException.printStackTrace();
+            }
+        };
+        schoolItem.addActionListener(schoolListener);
+
+        ActionListener tf2Listener = e -> {
+            /*
+            try {
+                Process p = Runtime.getRuntime().exec("D:\\Steam\\steamapps\\common\\Team Fortress 2\\hl2.exe");
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+             */
+            try {
+                desktop.browse(new URI("https://uncletopia.com/servers"));
+                desktop.browse(new URI("steam://rungameid/440"));
+            } catch (IOException | URISyntaxException ioException) {
+                ioException.printStackTrace();
+            }
+        };
+        tf2Item.addActionListener(tf2Listener);
+
         //Add components to pop-up menu
-        popup.add(aboutItem);
-        popup.addSeparator();
-        popup.add(cb1);
-        popup.add(cb2);
-        popup.addSeparator();
-        popup.add(displayMenu);
-        displayMenu.add(errorItem);
-        displayMenu.add(warningItem);
-        displayMenu.add(infoItem);
-        displayMenu.add(noneItem);
+        //popup.add(aboutItem);
+        //popup.addSeparator();
+        //popup.add(cb1);
+        //popup.add(cb2);
+        //popup.addSeparator();
+        popup.add(shortcutsMenu);
+        shortcutsMenu.add(emailItem);
+        shortcutsMenu.add(schoolItem);
+        shortcutsMenu.add(tf2Item);
         popup.add(exitItem);
 
         trayIcon.setPopupMenu(popup);
